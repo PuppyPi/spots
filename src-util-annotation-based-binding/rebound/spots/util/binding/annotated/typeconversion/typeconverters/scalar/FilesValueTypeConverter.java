e5a204321e12c67d4b64e5fbcf210c9fe97cd568
PuppyPi <puppyofcodez@gmail.com>
2023-01-10 08:18:49 +0000

    You know I never used singletonOnly() but now that I am I notice it doesn't make sense for its field to be typed as List X3

--- src-util-annotation-based-binding/rebound/spots/util/binding/annotated/typeconversion/typeconverters/scalar/FilesValueTypeConverter.java
+++ src-util-annotation-based-binding/rebound/spots/util/binding/annotated/typeconversion/typeconverters/scalar/FilesValueTypeConverter.java
@@ -4,9 +4,11 @@
  */
 package rebound.spots.util.binding.annotated.typeconversion.typeconverters.scalar;
 
+import static rebound.util.collections.BasicCollectionUtilities.*;
 import java.lang.annotation.Annotation;
 import java.util.List;
 import rebound.spots.ActionBeanContext;
+import rebound.spots.traditionaljee.util.fileupload.FileValue;
 import rebound.spots.traditionaljee.util.fileupload.MultipartFilter;
 import rebound.spots.util.binding.annotated.typeconversion.TypeConverter;
 
@@ -15,9 +17,15 @@ implements TypeConverter<FormBoundFiles>
 {
 	public Object convert(ActionBeanContext context, Class destination, FormBoundFiles specifier, Annotation[] environment) throws Exception
 	{
-		if (!List.class.isAssignableFrom(destination))
+		Class expectedSuperType = specifier.singletonOnly() ? FileValue.class : List.class;
+		if (!expectedSuperType.isAssignableFrom(destination))
 			throw new ClassCastException();
 		
-		return context.getRequest().getAttribute(MultipartFilter.STORAGE_ATTRIBUTE_PREFIX+specifier.value());
+		List<FileValue<?>> filesvalue = (List<FileValue<?>>) context.getRequest().getAttribute(MultipartFilter.STORAGE_ATTRIBUTE_PREFIX+specifier.value());
+		
+		if (specifier.singletonOnly())
+			return getSingleElementOrNullIfNone(filesvalue);  //the AcceptFilter should have made sure more than one file wasn't allowed
+		else
+			return filesvalue;
 	}
 }
