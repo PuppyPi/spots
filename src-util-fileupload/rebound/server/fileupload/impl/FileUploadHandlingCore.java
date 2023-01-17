f26d86f11d886c8fec89c5567aed2ab8b343d66c
PuppyPi <puppyofcodez@gmail.com>
2023-01-17 01:43:15 +0000

    REFACTORED IT!!  \:DDD/

---
+++ src-util-fileupload/rebound/server/fileupload/impl/FileUploadHandlingCore.java
@@ -0,0 +1,372 @@
+package rebound.server.fileupload.impl;
+
+import static java.util.Collections.*;
+import java.awt.datatransfer.MimeTypeParseException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
+import rebound.io.util.TextIOUtilities;
+import rebound.server.fileupload.AcceptFilter;
+import rebound.server.fileupload.Datastore;
+import rebound.server.fileupload.FileValue;
+import rebound.server.fileupload.fromothers.MimeTypeParameterList;
+import rebound.server.fileupload.fromothers.MultipartStream;
+import rebound.util.Either;
+import rebound.util.MIMEHeaders;
+import rebound.util.MIMEHeaders.Header;
+import rebound.util.container.ContainerInterfaces.ObjectContainer;
+import rebound.util.container.SimpleContainers.SimpleObjectContainer;
+import rebound.util.functional.FunctionInterfaces.UnaryProcedure;
+import rebound.util.functional.throwing.FunctionalInterfacesThrowingCheckedExceptionsStandard.RunnableThrowingIOException;
+
+public class FileUploadHandlingCore
+{
+	public static boolean isRequestTypeToBeHandled(HttpServletRequest request)
+	{
+		//Only intercept the request if it's multipart, and thus unintelligible to the servlet container
+		return request.getContentType() != null && request.getContentType().startsWith("multipart");
+	}
+	
+	
+	
+	/**
+	 * @return the Form Parameters read (not just necessarily files, also other things too!!)  Will be null if and only if kill400 was called! otherwise a list that may or may not be empty (technically a valid MIME Multipart Message doesn't have to have an actual file upload in it!)
+	 */
+	public static @Nullable <D> Map<String, List<Either<String, FileValue<D>>>> handle(HttpServletRequest request, AcceptFilter acceptFilter, Datastore<D> datastore, RunnableThrowingIOException kill400) throws IOException
+	{
+		ObjectContainer<Map<String, List<Either<String, FileValue<D>>>>> formParametersFoundC = new SimpleObjectContainer<>();
+		
+		byte[] boundary = null;
+		{
+			MimeTypeParameterList params = null;
+			{
+				String paramData = null;
+				{
+					String contentType = request.getContentType();
+					
+					int semicolonIndex = contentType.indexOf(';');
+					if (semicolonIndex != -1)
+						paramData = contentType.substring(semicolonIndex);
+					else
+						paramData = "";
+				}
+				
+				if (paramData.length() > 0)
+				{
+					try
+					{
+						params = new MimeTypeParameterList(paramData);
+					}
+					catch (MimeTypeParseException exc)
+					{
+						//Syntax Error
+						kill400.run();
+						return null;
+					}
+				}
+				else
+				{
+					params = null;
+				}
+			}
+			
+			
+			String cbound = null;
+			if (params != null)
+				cbound = params.get("boundary");
+			
+			if (cbound == null || cbound.length() == 0)
+			{
+				//No Boundary!
+				kill400.run();
+				return null;
+			}
+			
+			boundary = cbound.getBytes("ASCII");
+		}
+		
+		
+		
+		MultipartStream multipartStream = new MultipartStream(request.getInputStream());
+		multipartStream.setBoundary(boundary);
+		
+		
+		boolean continewe = multipartStream.skipPreamble();
+		
+		while (continewe)
+		{
+			//Process encapsulation
+			MIMEHeaders headers = null;
+			{
+				try
+				{
+					headers = MIMEHeaders.parse(multipartStream.readHeaders());
+				}
+				catch (MimeTypeParseException exc)
+				{
+					kill400.run();
+					return null;
+				}
+			}
+			
+			
+			Header contentDispositionHeader = headers.getHeader("Content-Disposition");
+			
+			if (contentDispositionHeader == null)
+			{
+				kill400.run();
+				return null;
+			}
+			else if (contentDispositionHeader.getValue().equals("form-data"))
+			{
+				final String formFieldName;
+				{
+					formFieldName = contentDispositionHeader.getParameterValue("name");
+					
+					if (formFieldName == null || formFieldName.length() == 0)
+					{
+						kill400.run();
+						return null;
+					}
+				}
+				
+				if (formFieldName != null && formFieldName.length() > 0)
+				{
+					UnaryProcedure<Either<String, FileValue<D>>> gotOne = thisOne ->
+					{
+						Map<String, List<Either<String, FileValue<D>>>> formParametersFound = formParametersFoundC.get();
+						
+						if (formParametersFound == null)
+						{
+							formParametersFound = new HashMap<>();
+							formParametersFoundC.set(formParametersFound);
+						}
+						
+						List<Either<String, FileValue<D>>> forThisField = formParametersFound.get(formFieldName);
+						
+						if (forThisField instanceof ArrayList)  //isMutable(forThisField)
+							forThisField.add(thisOne);
+						else
+						{
+							if (forThisField == null)
+								forThisField = singletonList(thisOne);
+							else
+							{
+								forThisField = new ArrayList<>();
+								forThisField.add(thisOne);
+							}
+							
+							formParametersFound.put(formFieldName, forThisField);
+						}
+					};
+					
+					
+					Header contentTypeHeader = headers.getHeader("Content-Type");
+					String contentType = (contentTypeHeader == null ? null : contentTypeHeader.getValue());  //Todo should this keep the mime parameters (like charset=...)?
+					
+					boolean mixed = "multipart/mixed".equals(contentType);
+					
+					if (!mixed && contentDispositionHeader.getParameterValue("filename") == null)
+					{
+						//Simple form data
+						{
+							String value = null;
+							{
+								String charset = null;
+								{
+									if (contentTypeHeader != null)
+										charset = contentTypeHeader.getParameterValue("charset");
+									if (charset == null)
+										charset = "UTF-8";
+								}
+								
+								value = new String(TextIOUtilities.readAll(new InputStreamReader(multipartStream.newInputStream(), charset)));
+							}
+							
+							//Store {name,value} in request.parameters
+							{
+								gotOne.f(Either.forA(value));
+							}
+						}
+					}
+					else
+					{
+						//File form data
+						{
+							//If this file will be the second+ file for the given form field
+							Map<String, List<Either<String, FileValue<D>>>> formParametersFound = formParametersFoundC.get();
+							boolean multipleFileValues = (formParametersFound != null && formParametersFound.containsKey(formFieldName)) || mixed;
+							
+							//Make sure it's not rejected (for mixed, this is just a once-over check of all files at once, because they share a lot of attributes)
+							if (!checkAccept(acceptFilter, request, formFieldName, !multipleFileValues, headers))
+								return null;
+							
+							
+							
+							//Download the data
+							
+							if (!mixed)
+							{
+								String filename = contentDispositionHeader.getParameterValue("filename");
+								
+								
+								//Skip empty file boxes
+								if (filename != null && !filename.isEmpty())
+								{
+									//Single file
+									InputStream dataIn = multipartStream.newInputStream();
+									
+									D filehandle = datastore.store(dataIn, -1, filename, contentType);
+									
+									if (filehandle == null)
+									{
+										dataIn.close();
+										multipartStream.discardBodyData();
+									}
+									
+									FileValue<D> value = new FileValue<>();
+									value.setContentType(contentType);
+									value.setFilename(filename);
+									value.setFilehandle(filehandle);
+									
+									gotOne.f(Either.forB(value));
+								}
+							}
+							
+							
+							else
+							{
+								//TODO Softcode a notification system for here ^^''
+								//		Root.getPostmaster().notifyUnexpectedEvent("--------------------------------------------------------------------------------------------------------------------------Nested Multiparts!-------------------------------------------------------------------------------------------------------------------------- just thought you ought ta know.");
+								
+								//Multiple files
+								byte[] filesBoundary = null;
+								{
+									String cbound = contentTypeHeader.getParameterValue("boundary");
+									if (cbound != null)
+										filesBoundary = cbound.getBytes("ASCII");
+									if (filesBoundary == null)
+									{
+										kill400.run();
+										return null;
+									}
+								}
+								
+								MultipartStream filesMultipartStream = new MultipartStream(multipartStream.newInputStream());
+								filesMultipartStream.setBoundary(filesBoundary);
+								
+								boolean filesContinewe = filesMultipartStream.skipPreamble();
+								
+								while (filesContinewe)
+								{
+									MIMEHeaders currFileHeaders = null;
+									{
+										String headerData = filesMultipartStream.readHeaders();
+										try
+										{
+											currFileHeaders = MIMEHeaders.parse(headerData);
+										}
+										catch (MimeTypeParseException exc)
+										{
+											kill400.run();
+										}
+									}
+									
+									String currFilename = null;
+									{
+										Header currContentDispositionHeader = currFileHeaders.getHeader("Content-Disposition");
+										if (currContentDispositionHeader != null)
+											currFilename = currContentDispositionHeader.getParameterValue("filename");
+									}
+									
+									//Skip empty file boxes
+									if (currFilename != null && currFilename.length() > 0)
+									{
+										//Make sure it's not rejected
+										if (!checkAccept(acceptFilter, request, formFieldName, false, currFileHeaders))
+											return null;
+										
+										String currContentType = currFileHeaders.getHeaderValue("Content-Type");
+										
+										InputStream currDataIn = multipartStream.newInputStream();
+										
+										D currFilehandle = datastore.store(currDataIn, -1, currFilename, currContentType);
+										
+										if (currFilehandle == null)
+										{
+											currDataIn.close();
+											multipartStream.discardBodyData();
+										}
+										
+										
+										FileValue<D> currValue = new FileValue<>();
+										currValue.setContentType(currContentType);
+										currValue.setFilename(currFilename);
+										currValue.setFilehandle(currFilehandle);
+										
+										gotOne.f(Either.forB(currValue));
+									}
+									
+									filesContinewe = filesMultipartStream.readBoundary();
+								}
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				//Ignore unsupported Content-Dispositions
+			}
+			
+			continewe = multipartStream.readBoundary();
+		}
+		
+		
+		
+		Map<String, List<Either<String, FileValue<D>>>> formParametersFound = formParametersFoundC.get();
+		return formParametersFound == null ? emptyMap() : formParametersFound;
+	}
+	
+	
+	
+	
+	
+	
+	protected static boolean checkAccept(AcceptFilter acceptFilter, HttpServletRequest request, String formFieldName, boolean singletonFile, MIMEHeaders headers) throws IOException
+	{
+		String filename = null;
+		{
+			Header cd = headers.getHeader("Content-Disposition");
+			if (cd != null)
+				filename = cd.getParameterValue("filename");
+		}
+		
+		boolean acc = false;
+		{
+			if (acceptFilter == null || filename == null)
+				acc = false;
+			else
+				acc = acceptFilter.accept(request, formFieldName, singletonFile, filename, headers.getHeaderValue("Content-Type"));
+		}
+		
+		if (!acc)
+		{
+			request.getInputStream().close();
+			System.err.println("MultipartFilter) ! File upload rejected:");
+			System.err.println("\tField name: \""+formFieldName+"\"");
+			System.err.println("\tFilename: \""+filename+"\"");
+			System.err.println("\tContent-Type: "+headers.getHeaderValue("Content-Type"));
+			System.err.println("\tMultiple files on field: "+!singletonFile);
+			System.err.println("\tContent-Length: "+headers.getHeaderValue("Content-Length"));
+		}
+		
+		return acc;
+	}
+}
--- src-util-fileupload/rebound/spots/traditionaljee/util/fileupload/MultipartFilter.java
+++ src-util-fileupload/rebound/spots/traditionaljee/util/fileupload/MultipartFilter.java
@@ -4,26 +4,22 @@
  */
 package rebound.spots.traditionaljee.util.fileupload;
 
-import java.awt.datatransfer.MimeTypeParseException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import rebound.io.util.TextIOUtilities;
 import rebound.server.fileupload.AcceptFilter;
 import rebound.server.fileupload.Datastore;
 import rebound.server.fileupload.FileValue;
-import rebound.server.fileupload.fromothers.MimeTypeParameterList;
-import rebound.server.fileupload.fromothers.MultipartStream;
+import rebound.server.fileupload.impl.FileUploadHandlingCore;
 import rebound.spots.traditionaljee.util.AbstractHTTPFilter;
 import rebound.spots.traditionaljee.util.MutableParameterServletRequest;
-import rebound.util.MIMEHeaders;
-import rebound.util.MIMEHeaders.Header;
+import rebound.util.Either;
 
 /**
  * Yes. Another multipart resolving filter. Get over it.<br>
@@ -42,8 +38,8 @@ public abstract class MultipartFilter<D>
 extends AbstractHTTPFilter
 {
 	public static final String STORAGE_ATTRIBUTE_PREFIX = "filesvalue_";
-//	public static final String ACCEPT_FILTER_ATTRIBUTE = "accept_files";
-//	public static final String DATASTORE_ATTRIBUTE = "datastore";
+	//	public static final String ACCEPT_FILTER_ATTRIBUTE = "accept_files";
+	//	public static final String DATASTORE_ATTRIBUTE = "datastore";
 	
 	
 	
@@ -52,287 +48,60 @@ extends AbstractHTTPFilter
 		MutableParameterServletRequest mutreq = null;
 		
 		//Only intercept the request if it's multipart, and thus unintelligible to the servlet container
-		if (request.getContentType() != null && request.getContentType().startsWith("multipart"))
+		if (FileUploadHandlingCore.isRequestTypeToBeHandled(request))
 		{
 			AcceptFilter acceptFilter = getAcceptFilter(request, response);
 			Datastore<D> datastore = getDatastore(request, response);
 			
-			byte[] boundary = null;
+			
+			Map<String, List<Either<String, FileValue<D>>>> r = FileUploadHandlingCore.handle(request, acceptFilter, datastore, () -> this.kill400(request, response));
+			
+			if (r == null)  //error
+				return;
+			else
 			{
-				MimeTypeParameterList params = null;
+				for (Entry<String, List<Either<String, FileValue<D>>>> e : r.entrySet())
 				{
-					String paramData = null;
-					{
-						String contentType = request.getContentType();
-						
-						int semicolonIndex = contentType.indexOf(';');
-						if (semicolonIndex != -1)
-							paramData = contentType.substring(semicolonIndex);
-						else
-							paramData = "";
-					}
+					String formFieldName = e.getKey();
 					
-					if (paramData.length() > 0)
+					for (Either<String, FileValue<D>> formParameterValue : e.getValue())
 					{
-						try
+						if (formParameterValue.isA())
 						{
-							params = new MimeTypeParameterList(paramData);
-						}
-						catch (MimeTypeParseException exc)
-						{
-							//Syntax Error
-							kill400(request, response);
-							return;
-						}
-					}
-					else
-					{
-						params = null;
-					}
-				}
-				
-				
-				String cbound = null;
-				if (params != null)
-					cbound = params.get("boundary");
-				
-				if (cbound == null || cbound.length() == 0)
-				{
-					//No Boundary!
-					kill400(request, response);
-					return;
-				}
-				
-				boundary = cbound.getBytes("ASCII");
-			}
-			
-			
-			
-			MultipartStream multipartStream = new MultipartStream(request.getInputStream());
-			multipartStream.setBoundary(boundary);
-			
-			
-			boolean continewe = multipartStream.skipPreamble();
-			
-			while (continewe)
-			{
-				//Process encapsulation
-				MIMEHeaders headers = null;
-				{
-					try
-					{
-						headers = MIMEHeaders.parse(multipartStream.readHeaders());
-					}
-					catch (MimeTypeParseException exc)
-					{
-						kill400(request, response);
-						return;
-					}
-				}
-				
-				
-				Header contentDispositionHeader = headers.getHeader("Content-Disposition");
-				
-				if (contentDispositionHeader == null)
-				{
-					kill400(request, response);
-					return;
-				}
-				else if (contentDispositionHeader.getValue().equals("form-data"))
-				{
-					String formFieldName = null;
-					{
-						formFieldName = contentDispositionHeader.getParameterValue("name");
-						if (formFieldName == null || formFieldName.length() == 0)
-						{
-							kill400(request, response);
-							return;
-						}
-					}
-					
-					if (formFieldName != null && formFieldName.length() > 0)
-					{
-						Header contentTypeHeader = headers.getHeader("Content-Type");
-						String contentType = (contentTypeHeader == null ? null : contentTypeHeader.getValue());  //Todo should this keep the mime parameters (like charset=...)?
-						
-						boolean mixed = "multipart/mixed".equals(contentType);
-						
-						if (!mixed && contentDispositionHeader.getParameterValue("filename") == null)
-						{
-							//Simple form data
+							//Store {name,value} in request.parameters
 							{
-								String value = null;
+								if (mutreq == null)
 								{
-									String charset = null;
-									{
-										if (contentTypeHeader != null)
-											charset = contentTypeHeader.getParameterValue("charset");
-										if (charset == null)
-											charset = "UTF-8";
-									}
-									
-									value = new String(TextIOUtilities.readAll(new InputStreamReader(multipartStream.newInputStream(), charset)));
+									//Acquire a parameter-mutable request
+									if (request instanceof MutableParameterServletRequest)
+										mutreq = (MutableParameterServletRequest)request;
+									else
+										mutreq = new SimpleMutableParameterServletRequestWrapper(request);
 								}
 								
-								//Store {name,value} in request.parameters
-								{
-									if (mutreq == null)
-									{
-										//Acquire a parameter-mutable request
-										if (request instanceof MutableParameterServletRequest)
-											mutreq = (MutableParameterServletRequest)request;
-										else
-											mutreq = new SimpleMutableParameterServletRequestWrapper(request);
-									}
-									
-									mutreq.putParameter(formFieldName, value);
-								}
+								mutreq.putParameter(formFieldName, formParameterValue.getValueIfA());
 							}
 						}
 						else
 						{
-							//File form data
+							final List<FileValue<D>> filesvalue;
 							{
-								final List<FileValue<D>> filesvalue;
-								{
-									String name = STORAGE_ATTRIBUTE_PREFIX+formFieldName;
-									Object o = request.getAttribute(name);
-									
-									if (o instanceof List)
-										filesvalue = (List<FileValue<D>>)o;
-									else
-									{
-										filesvalue = new ArrayList<FileValue<D>>();
-										request.setAttribute(name, filesvalue);
-									}
-								}
+								String name = STORAGE_ATTRIBUTE_PREFIX+formFieldName;
+								Object o = request.getAttribute(name);
 								
-								//If this file will be the second+ file for the given form field
-								boolean multipleFileValues = !filesvalue.isEmpty() || mixed;
-								
-								//Make sure it's not rejected (for mixed, this is just a once-over check of all files at once, because they share a lot of attributes)
-								if (!checkAccept(acceptFilter, request, response, formFieldName, !multipleFileValues, headers))
-									return;
-								
-								
-								
-								//Download the data
-								
-								if (!mixed)
-								{
-									String filename = contentDispositionHeader.getParameterValue("filename");
-									
-									
-									//Skip empty file boxes
-									if (filename != null && filename.length() > 0)
-									{
-										//Single file
-										InputStream dataIn = multipartStream.newInputStream();
-										
-										D filehandle = datastore.store(dataIn, -1, filename, contentType);
-										
-										if (filehandle == null)
-										{
-											dataIn.close();
-											multipartStream.discardBodyData();
-										}
-										
-										FileValue<D> value = new FileValue<>();
-										value.setContentType(contentType);
-										value.setFilename(filename);
-										value.setFilehandle(filehandle);
-										
-										filesvalue.add(value);
-									}
-								}
-								
-								
+								if (o instanceof List)
+									filesvalue = (List<FileValue<D>>)o;
 								else
 								{
-									//TODO Softcode a notification system for here ^^''
-									//		Root.getPostmaster().notifyUnexpectedEvent("--------------------------------------------------------------------------------------------------------------------------Nested Multiparts!-------------------------------------------------------------------------------------------------------------------------- just thought you ought ta know.");
-									
-									//Multiple files
-									byte[] filesBoundary = null;
-									{
-										String cbound = contentTypeHeader.getParameterValue("boundary");
-										if (cbound != null)
-											filesBoundary = cbound.getBytes("ASCII");
-										if (filesBoundary == null)
-										{
-											kill400(request, response);
-											return;
-										}
-									}
-									
-									MultipartStream filesMultipartStream = new MultipartStream(multipartStream.newInputStream());
-									filesMultipartStream.setBoundary(filesBoundary);
-									
-									boolean filesContinewe = filesMultipartStream.skipPreamble();
-									
-									while (filesContinewe)
-									{
-										MIMEHeaders currFileHeaders = null;
-										{
-											String headerData = filesMultipartStream.readHeaders();
-											try
-											{
-												currFileHeaders = MIMEHeaders.parse(headerData);
-											}
-											catch (MimeTypeParseException exc)
-											{
-												kill400(request, response);
-											}
-										}
-										
-										String currFilename = null;
-										{
-											Header currContentDispositionHeader = currFileHeaders.getHeader("Content-Disposition");
-											if (currContentDispositionHeader != null)
-												currFilename = currContentDispositionHeader.getParameterValue("filename");
-										}
-										
-										//Skip empty file boxes
-										if (currFilename != null && currFilename.length() > 0)
-										{
-											//Make sure it's not rejected
-											if (!checkAccept(acceptFilter, request, response, formFieldName, false, currFileHeaders))
-												return;
-											
-											String currContentType = currFileHeaders.getHeaderValue("Content-Type");
-											
-											InputStream currDataIn = multipartStream.newInputStream();
-											
-											D currFilehandle = datastore.store(currDataIn, -1, currFilename, currContentType);
-											
-											if (currFilehandle == null)
-											{
-												currDataIn.close();
-												multipartStream.discardBodyData();
-											}
-											
-											
-											FileValue<D> currValue = new FileValue<>();
-											currValue.setContentType(currContentType);
-											currValue.setFilename(currFilename);
-											currValue.setFilehandle(currFilehandle);
-											
-											filesvalue.add(currValue);
-										}
-										
-										filesContinewe = filesMultipartStream.readBoundary();
-									}
+									filesvalue = new ArrayList<FileValue<D>>();
+									request.setAttribute(name, filesvalue);
 								}
 							}
+							
+							filesvalue.add(formParameterValue.getValueIfB());
 						}
 					}
 				}
-				else
-				{
-					//Ignore unsupported Content-Dispositions
-				}
-				
-				continewe = multipartStream.readBoundary();
 			}
 		}
 		else
@@ -349,38 +118,6 @@ extends AbstractHTTPFilter
 		request.getInputStream().close();
 		response.sendError(400);
 		response.flushBuffer();
-	}
-	
-	private boolean checkAccept(AcceptFilter acceptFilter, HttpServletRequest request, HttpServletResponse response, String formFieldName, boolean singletonFile, MIMEHeaders headers) throws IOException
-	{
-		String filename = null;
-		{
-			Header cd = headers.getHeader("Content-Disposition");
-			if (cd != null)
-				filename = cd.getParameterValue("filename");
-		}
-		
-		boolean acc = false;
-		{
-			if (acceptFilter == null || filename == null)
-				acc = false;
-			else
-				acc = acceptFilter.accept(request, formFieldName, singletonFile, filename, headers.getHeaderValue("Content-Type"));
-		}
-		
-		if (!acc)
-		{
-			response.getOutputStream().close();
-			request.getInputStream().close();
-			System.err.println("MultipartFilter) ! File upload rejected:");
-			System.err.println("\tField name: \""+formFieldName+"\"");
-			System.err.println("\tFilename: \""+filename+"\"");
-			System.err.println("\tContent-Type: "+headers.getHeaderValue("Content-Type"));
-			System.err.println("\tMultiple files on field: "+!singletonFile);
-			System.err.println("\tContent-Length: "+headers.getHeaderValue("Content-Length"));
-		}
-		
-		return acc;
 	}
 	
 	
