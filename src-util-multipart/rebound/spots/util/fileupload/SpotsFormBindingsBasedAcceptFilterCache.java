b49dabc8b71bc82f7b802bb4aa307237894ba576
PuppyPi <puppyofcodez@gmail.com>
2023-01-18 02:10:02 +0000

    Ahaaaaa!  Great performance!  \:D/

---
+++ src-util-multipart/rebound/spots/util/fileupload/SpotsFormBindingsBasedAcceptFilterCache.java
@@ -0,0 +1,42 @@
+package rebound.spots.util.fileupload;
+
+import static rebound.util.collections.CollectionUtilities.*;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import rebound.server.fileupload.AcceptFilter;
+import rebound.server.fileupload.util.NullAcceptFilter;
+import rebound.spots.util.binding.annotated.typeconversion.typeconverters.scalar.FormBoundFiles;
+import rebound.util.collections.FilterAwayReturnPath;
+
+public class SpotsFormBindingsBasedAcceptFilterCache
+{
+	protected Map<Class, AcceptFilter> exhaustiveEagerCache;
+	
+	public SpotsFormBindingsBasedAcceptFilterCache(Iterable<Class> exhaustiveListOfAllActionBeanClasses)
+	{
+		exhaustiveEagerCache = maptodictSameKeys(c ->
+		{
+			List<FormBoundFiles> b = SpotsFormBindingsBasedAcceptFilter.getFileBindings(c);
+			
+			if (b.isEmpty())
+				throw FilterAwayReturnPath.I;
+			else
+				return new SpotsFormBindingsBasedAcceptFilter(b);
+			
+		}, asSetUniqueifying(exhaustiveListOfAllActionBeanClasses));
+	}
+	
+	
+	public @Nullable AcceptFilter getAcceptFilterForActionBeanClassOrNull(Class actionBeanClass)
+	{
+		return exhaustiveEagerCache.get(actionBeanClass);
+	}
+	
+	public @Nonnull AcceptFilter getAcceptFilterForActionBeanClass(Class actionBeanClass)
+	{
+		AcceptFilter r = getAcceptFilterForActionBeanClassOrNull(actionBeanClass);
+		return r == null ? NullAcceptFilter.I : r;
+	}
+}
