193e37b2433a11c711de90e431eb9058b35452e1
PuppyPi <puppyofcodez@gmail.com>
2023-01-18 01:30:44 +0000

    Moved in from eCoops to this more general project! :D

---
+++ src-util-multipart/rebound/spots/util/fileupload/SpotsFormBindingsBasedAcceptFilter.java
@@ -0,0 +1,67 @@
+package rebound.spots.util.fileupload;
+
+import static rebound.util.collections.CollectionUtilities.*;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletRequest;
+import rebound.server.fileupload.AcceptFilter;
+import rebound.spots.util.binding.annotated.typeconversion.typeconverters.scalar.FormBoundFiles;
+import rebound.util.AngryReflectionUtility;
+
+public class SpotsFormBindingsBasedAcceptFilter
+implements AcceptFilter
+{
+	protected List<FormBoundFiles> bindings;
+	
+	public SpotsFormBindingsBasedAcceptFilter(List<FormBoundFiles> bindings)
+	{
+		this.bindings = bindings;
+	}
+	
+	public SpotsFormBindingsBasedAcceptFilter(Class actionBeanClass)
+	{
+		this(getFileBindings(actionBeanClass));
+	}
+	
+	
+	public boolean accept(ServletRequest req, String formName, boolean single, String filename, String contentType)
+	{
+		for (FormBoundFiles b : bindings)
+		{
+			if (b.value().equals(formName))
+			{
+				if (b.singletonOnly() && !single)
+					return false;
+				
+				//Filename and contentType don't affect acceptibility
+				return true;
+			}
+		}
+		
+		//No bindings for form field
+		return false;
+	}
+	
+	
+	
+	
+	
+	public static List<FormBoundFiles> getFileBindings(Class actionBeanClass)
+	{
+		Field[] fields = AngryReflectionUtility.getAllFields(actionBeanClass);
+		
+		List<FormBoundFiles> bindings = new ArrayList<>();
+		{
+			for (Field f : fields)
+			{
+				if (f.isAnnotationPresent(FormBoundFiles.class))
+				{
+					bindings.add(f.getAnnotation(FormBoundFiles.class));
+				}
+			}
+		}
+		
+		return trimmed(bindings);
+	}
+}
